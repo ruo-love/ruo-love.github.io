09e00c47af26511547bb46da2d7c073f6a236317
zero <zl18180276692@outlook.com>
2023-09-15 14:51:21 +0800

    feat

--- .vitepress/config/sidebar/webpack.js
+++ .vitepress/config/sidebar/webpack.js
@@ -3,39 +3,12 @@ module.exports = [
     text: "基础",
     link: "/articles/project/webpack/intro/",
     items: [
-        { text: "base", link: "/articles/project/webpack/base/base" },
-        { text: "config", link: "/articles/project/webpack/base/config" },
-        { text: "development", link: "/articles/project/webpack/base/development" },
-        { text: "css", link: "/articles/project/webpack/base/css" },
-        { text: "image", link: "/articles/project/webpack/base/image" },
-        { text: "output", link: "/articles/project/webpack/base/output" },
-        { text: "clean", link: "/articles/project/webpack/base/clean" },
-        { text: "font", link: "/articles/project/webpack/base/font" },
-        { text: "other", link: "/articles/project/webpack/base/other" },
-        { text: "javascript", link: "/articles/project/webpack/base/javascript" },
-        { text: "html", link: "/articles/project/webpack/base/html" },
-        { text: "server", link: "/articles/project/webpack/base/server" },
-        { text: "production", link: "/articles/project/webpack/base/production" },
-        { text: "optimizeCss", link: "/articles/project/webpack/base/optimizeCss" },
-        { text: "minifyHtml", link: "/articles/project/webpack/base/minifyHtml" },
-        { text: "summary", link: "/articles/project/webpack/base/summary" },
-    ],
-  },
-  {
-    text: "高级",
-    link: "/articles/project/webpack/intro/",
-    items: [
-      { text: "base", link: "/articles/project/webpack/intro/base" },
-      { text: "config", link: "/articles/project/webpack/intro/config" },
-    ],
-  },
-  {
-    text: "项目",
-    link: "/articles/project/webpack/intro/",
-    items: [
       { text: "base", link: "/articles/project/webpack/base/base" },
       { text: "config", link: "/articles/project/webpack/base/config" },
-      { text: "development", link: "/articles/project/webpack/base/development" },
+      {
+        text: "development",
+        link: "/articles/project/webpack/base/development",
+      },
       { text: "css", link: "/articles/project/webpack/base/css" },
       { text: "image", link: "/articles/project/webpack/base/image" },
       { text: "output", link: "/articles/project/webpack/base/output" },
@@ -46,17 +19,53 @@ module.exports = [
       { text: "html", link: "/articles/project/webpack/base/html" },
       { text: "server", link: "/articles/project/webpack/base/server" },
       { text: "production", link: "/articles/project/webpack/base/production" },
-      { text: "optimizeCss", link: "/articles/project/webpack/base/optimizeCss" },
+      {
+        text: "optimizeCss",
+        link: "/articles/project/webpack/base/optimizeCss",
+      },
       { text: "minifyHtml", link: "/articles/project/webpack/base/minifyHtml" },
       { text: "summary", link: "/articles/project/webpack/base/summary" },
     ],
   },
   {
-    text: "原理",
-    link: "/articles/project/webpack/intro/",
+    text: "高级",
+    link: "/articles/project/webpack/senior/",
     items: [
-      { text: "base", link: "/articles/project/webpack/intro/base" },
-      { text: "config", link: "/articles/project/webpack/intro/config" },
+      {
+        text: "提升开发体验",
+        link: "/articles/project/webpack/senior/enhanceExperience",
+      },
+      {
+        text: "提升打包构建速度",
+        link: "/articles/project/webpack/senior/liftingSpeed",
+      },
+      {
+        text: "优化代码运行性能",
+        link: "/articles/project/webpack/senior/optimizePerformance",
+      },
+      {
+        text: "减少代码体积",
+        link: "/articles/project/webpack/senior/reduceVolume",
+      },
+      { text: "总结", link: "/articles/project/webpack/senior/summary" },
+    ],
+  },
+  {
+    text: "项目",
+    link: "/articles/project/webpack/project/",
+    items: [
+      { text: "react", link: "/articles/project/webpack/project/react-cli" },
+      { text: "vue", link: "/articles/project/webpack/project/vue-cli" },
+      { text: "总结", link: "/articles/project/webpack/project/summary" },
+    ],
+  },
+  {
+    text: "原理",
+    link: "/articles/project/webpack/origin/",
+    items: [
+      { text: "loader", link: "/articles/project/webpack/origin/loader" },
+      { text: "plugin", link: "/articles/project/webpack/origin/plugin" },
+      { text: "总结", link: "/articles/project/webpack/origin/summary" },
     ],
   },
 ];
