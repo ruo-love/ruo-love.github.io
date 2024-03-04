f537aedf2578f67b76673fda8b2cafa024ab1526
zero <zl18180276692@outlook.com>
2024-03-04 16:35:09 +0800

    feat: update

--- .vitepress/config/sidebar/articles.js
+++ .vitepress/config/sidebar/articles.js
@@ -1,56 +1,63 @@
 module.exports = [
-  {
-    text: "Javascript",
-    items: [],
-  },
-  {
-    text: "Vue",
-    items: [],
-  },
-  {
-    text: "React",
-    items: [],
-  },
+  // {
+  //   text: "Javascript",
+  //   items: [],
+  // },
+  // {
+  //   text: "Vue",
+  //   items: [],
+  // },
+  // {
+  //   text: "React",
+  //   items: [],
+  // },
   {
     text: "小程序",
     items: [
+      {
+        text: "Taro自定底部状态栏",
+        link: "https://blog.csdn.net/SAXX2/article/details/136454928?spm=1001.2014.3001.5502",
+      },
+      {
+        text: "打开地图选择位置信息",
+        link: "https://blog.csdn.net/SAXX2/article/details/136454169?spm=1001.2014.3001.5502",
+      },
       { text: "机器人自动部署", link: "/articles/miniprocedure/" },
-      { text: "分包", link: "/articles/" },
     ],
   },
   {
     text: "前端工程化",
     items: [
       { text: "Webpack", link: "/articles/project/webpack/" },
-      { text: "Vite", link: "/works/introduction" },
+      // { text: "Vite", link: "/works/introduction" },
     ],
   },
-  {
-    text: "服务器",
-    items: [
-      { text: "node", link: "/articles/servers/" },
-      { text: "express", link: "/articles/introduction" },
-      { text: "koa2", link: "/articles/getting-started" },
-    ],
-  },
-  {
-    text: "数据库",
-    items: [
-      { text: "MySql", link: "/articles/db/" },
-      { text: "Mongodb", link: "/articles/introduction" },
-    ],
-  },
-  {
-    text: "Flutter",
-    items: [{ text: "创建工程", link: "/articles/flutter/" }],
-  },
+  // {
+  //   text: "服务器",
+  //   items: [
+  //     { text: "node", link: "/articles/servers/" },
+  //     { text: "express", link: "/articles/introduction" },
+  //     { text: "koa2", link: "/articles/getting-started" },
+  //   ],
+  // },
+  // {
+  //   text: "数据库",
+  //   items: [
+  //     { text: "MySql", link: "/articles/db/" },
+  //     { text: "Mongodb", link: "/articles/introduction" },
+  //   ],
+  // },
+  // {
+  //   text: "Flutter",
+  //   items: [{ text: "创建工程", link: "/articles/flutter/" }],
+  // },
 
   {
     text: "Python",
     items: [
-      { text: "爬虫", link: "/articles/python/" },
-      { text: "自动化脚本", link: "/articles/introduction" },
-      { text: "数据可视化", link: "/articles/getting-started" },
+      { text: "爬虫——贝壳二手房", link: "https://blog.csdn.net/SAXX2/article/details/135648172?spm=1001.2014.3001.5502" },
+      // { text: "自动化脚本", link: "/articles/introduction" },
+      // { text: "数据可视化", link: "/articles/getting-started" },
     ],
   },
 ];
