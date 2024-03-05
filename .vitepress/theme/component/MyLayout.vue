347a09a1ded0c6a139b5115c3a5333a96e6d896f
zero <zl18180276692@outlook.com>
2024-03-05 09:53:01 +0800

    feat: update

--- .vitepress/theme/component/MyLayout.vue
+++ .vitepress/theme/component/MyLayout.vue
@@ -1,14 +1,18 @@
 <!--.vitepress/theme/MyLayout.vue-->
 <script setup>
 import DefaultTheme from "vitepress/theme";
+import { useData } from "vitepress";
+const { isDark } = useData();
 const { Layout } = DefaultTheme;
+console.log(isDark);
 </script>
 
 <template>
   <Layout>
     <Content />
-    <template #home-hero-image
-      ><img src="../images/banner.png" /></template
+    <template #home-hero-image>
+      <img v-show="!isDark" src="/banner.png" />
+      <h1 style="color: #fff; font-size: 30px;letter-spacing: 1.15rem;" v-show="isDark">该睡觉啦！晚安</h1></template
     >
   </Layout>
 </template>
